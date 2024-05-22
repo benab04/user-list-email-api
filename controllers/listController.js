79c728d3dd324d1be8ad8b9708a045e4e7c6de38
benab04 <benabrahambiju@gmail.com>
2024-05-22 11:35:24 +0530

    add change fallback option

--- controllers/listController.js
+++ controllers/listController.js
@@ -41,3 +41,27 @@ exports.getDashboard = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
+exports.updateFallbacks = async (req, res) => {
+  const listId = req.params.id;
+  const { fallbacks } = req.body;
+
+  try {
+    const list = await List.findById(listId);
+    if (!list) {
+      return res.status(404).send("List not found");
+    }
+
+    list.customProperties.forEach((property) => {
+      if (fallbacks[property.title]) {
+        property.fallback = fallbacks[property.title];
+      }
+    });
+
+    await list.save();
+
+    res.redirect(`/lists/${listId}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -10,5 +10,6 @@ router.get("/", listController.getDashboard);
 router.post("/", listController.createList);
 router.get("/:id", listController.getList);
 router.delete("/:id", listController.deleteList);
+router.post("/:id/fallbacks", listController.updateFallbacks);
 
 module.exports = router;
